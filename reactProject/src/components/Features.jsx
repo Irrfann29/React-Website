3805f2af8702a3fc1561ba736cc1429435352228
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-09-30 07:17:52 +0530

    Minor Changes

--- reactProject/src/components/Features.jsx
+++ reactProject/src/components/Features.jsx
@@ -10,8 +10,8 @@ const Features = () => {
           <div className='w-[14px] h-[14px] rounded-full bg-amber-50'></div>
           <div>Salience Labs</div>
           </div>
-        <div className='h-[70vh] w-[98%] bg-amber-300 mt-8 rounded-2xl'>
-          <img className='bg-cover object-cover w-[100%] h-[100%] rounded-2xl bg-center ' src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png" alt="" />
+        <div className='h-[70vh] w-[98%]  mt-8 rounded-2xl'>
+          <img className=' bg-cover object-cover w-[100%] h-[100%] rounded-2xl bg-center ' src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png" alt="" />
         </div>
         <div className='uppercase pt-8 flex gap-4'>
             <div className='border-1 px-5 py-2 rounded-3xl border-gray-400'>Brand Identity </div>
@@ -25,8 +25,8 @@ const Features = () => {
           <div className='w-[14px] h-[14px] rounded-full bg-amber-50'></div>
           <div>Madellia experience</div>
           </div>
-          <div className='h-[70vh] w-[98%] bg-amber-300 mt-8 rounded-2xl'>
-          <img className='bg-cover object-cover w-[100%] h-[100%] rounded-2xl bg-center ' src="https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png" alt="" />
+          <div className='cards h-[70vh] w-[98%] mt-8 rounded-2xl overflow-hidden'>
+          <img className='imagwa object-cover w-[100%] h-[100%] rounded-2xl ' src="https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png" alt="" />
         </div>
           <div className='uppercase pt-8 flex gap-4'>
             <div className='border-1 px-5 py-2 rounded-3xl border-gray-400'>Brand Identity </div>
