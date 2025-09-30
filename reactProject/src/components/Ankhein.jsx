0c03ed48daa6914054558cad7e240e363a290491
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-09-30 21:16:28 +0530

    Minor Changes

--- reactProject/src/components/Ankhein.jsx
+++ reactProject/src/components/Ankhein.jsx
@@ -84,7 +84,7 @@ export default function Ankhein() {
     <>
     
       
-        <div className="flex items-center justify-center h-screen space-x-8 bg-cover bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')] bg-center">
+        <div className="flex items-center justify-center h-screen space-x-8 bg-cover bg-[url('https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center">
         <Eye />
       <Eye />
       </div>
